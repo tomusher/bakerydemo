e945afd0f809fd49a9bbe436b617db794a22ea7c
Tom Usher <tom@tomusher.com>
2025-11-13 16:20:00 +0000

    Simplify entrypoint temporarily

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = '/venv/bin/uwsgi' ]; then
-    /venv/bin/python manage.py migrate --noinput
-fi
-
-if [ "x$DJANGO_LOAD_INITIAL_DATA" = 'xon' ]; then
-	/venv/bin/python manage.py load_initial_data
-fi
+/venv/bin/python manage.py migrate --noinput
+/venv/bin/python manage.py load_initial_data
 
 exec "$@"
