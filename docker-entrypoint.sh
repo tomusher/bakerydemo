546cfddf5675f881603e40273cd8be0894e0c7a2
Tom Usher <tom@tomusher.com>
2025-11-13 15:11:40 +0000

    Remove postgres readiness check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
 
-until psql $DATABASE_URL -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing"
-
 if [ "$1" = '/venv/bin/uwsgi' ]; then
     /venv/bin/python manage.py migrate --noinput
 fi
